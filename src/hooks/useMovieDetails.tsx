09cd0ff1a9275aa124578e929f89018aca853aeb
Daniel Ortega <danielortegacareaga@gmail.com>
2023-01-27 20:23:59 -0600

    feat: Refactor favorites  movies

--- src/hooks/useMovieDetails.tsx
+++ src/hooks/useMovieDetails.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useState, useEffect} from 'react';
 import moviesApi from '../api/moviesApi';
 import {Cast, CreditsResponse} from '../interfaces/credits.interfaces';
